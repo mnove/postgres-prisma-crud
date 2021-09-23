d9b2bf0ba710b701625e74e8bd1b4f7145be6b1f
Marcello Novelli <62885851+mnove@users.noreply.github.com>
2021-09-23 09:52:15 +0200

    add user routes (#1)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,10 @@ const userController = require("../controllers/userContoller");
 // routes are scoped to "api/organization/" in app.js
 
 router.get("/", userController.getAll); // Get all users from the DB
+router.get("/:userId", userController.getOne); // Get one user from the DB
+router.post("/", userController.createUser); // Create a new user
+router.put("/", userController.updateUser); //
+router.delete("/:userId", userController.deleteUser); //
 // router.post("/", userController.createUser); // Create a new organization
 // router.delete("/", userController.deleteUserNoParam);
 // router.delete("/:organizationId", userController.deleteUser); // Delete a user
